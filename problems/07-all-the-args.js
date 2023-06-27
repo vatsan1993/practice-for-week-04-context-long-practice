c52dbe285948a8dadaa18834d6539866fe2fe0d1
Srivatsan <vatsan1993@gmail.com>
2023-06-27 14:40:02 +0530

    finished 7th program

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,7 +1,17 @@
 function allTheArgs(func, ...args) {
   // Your code here
+  return func.bind(this, ...args);
 }
+// const bow = (...names) => {
+//   return 'You bowed to ' + names.join(' and ');
+// };
 
+// const adder = (...nums) => nums.reduce((num, sum) => sum + num);
+
+// let addFive = allTheArgs(adder, 5);
+// let bowSandy = allTheArgs(bow, 'Sandy');
+// console.log(addFive());
+// console.log(bowSandy());
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
