ff8bf2cb185dfb8f60e69a6fbc326d0cb9aa5bbd
Srivatsan <vatsan1993@gmail.com>
2023-06-27 15:14:11 +0530

    finished 10th program

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,19 @@
 // Your code here
-class PartyPlanner {}
+class PartyPlanner {
+  constructor() {
+    this.guestList = [];
+  }
+  addToGuestList(name) {
+    this.guestList.push(name);
+  }
+  throwParty() {
+    if (this.guestList.length === 0) {
+      return 'Gotta add people to the guest list';
+    } else {
+      return 'Welcome to the party ' + this.guestList.join(' and ');
+    }
+  }
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
